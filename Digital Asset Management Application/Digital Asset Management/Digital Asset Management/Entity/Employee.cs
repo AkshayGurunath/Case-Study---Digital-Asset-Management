b363d8a30bf5b83a034092fb29545b6cb154eb8a
AkshayParab <117382696+AkshayGurunath@users.noreply.github.com>
2024-10-11 23:33:48 +0530

    Case Study

---
+++ Digital Asset Management Application/Digital Asset Management/Digital Asset Management/Entity/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Entity
+{
+    public class Employee
+    {
+        public int EmployeeId;
+        public string Name;
+        public string Department;
+        public string Email;
+        public string Password;
+    }
+}
