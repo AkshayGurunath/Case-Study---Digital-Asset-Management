ad98544aa229a045e8ae0cf463aa0e495eca88ad
AkshayParab <117382696+AkshayGurunath@users.noreply.github.com>
2024-10-14 15:50:19 +0530

    Add files via upload

---
+++ Digital Asset Management App/DigitalAssetManagement.Util/DbConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace DigitalAssetManagement.Util
+{
+    public static class DBUtil
+    {
+        readonly static SqlConnection sqlConnection;
+        static DBUtil()
+        {
+            sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+        }
+
+        public static SqlConnection SqlConnection => sqlConnection;
+    }
+    //public static class DbUtil
+    //{
+    //    public static SqlConnection GetDBConnection()
+    //    {
+
+    //        SqlConnection connection = new SqlConnection(connectionString);
+    //        sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+    //        return connection;
+    //    }
+
+    //}
+
+}
