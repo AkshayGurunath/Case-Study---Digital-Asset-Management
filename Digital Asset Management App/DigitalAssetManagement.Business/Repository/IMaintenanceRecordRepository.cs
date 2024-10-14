0a466208f10c67eff3ae714367688c88166326d7
AkshayParab <117382696+AkshayGurunath@users.noreply.github.com>
2024-10-14 15:47:46 +0530

    Add files via upload

---
+++ Digital Asset Management App/DigitalAssetManagement.Business/Repository/IMaintenanceRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿using DigitalAssetManagement.Exceptions;
+using DigitalAssetManagement.Util;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalAssetManagement.Business.Repository
+{
+    public interface IMaintenanceRecordRepository
+    {
+        bool PerformMaintenance(int assetId, DateTime maintenanceDate, string description, double cost);
+    }
+}
+
+
+    
